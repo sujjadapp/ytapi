30656ad61e153efd639338826e5b32b81c926126
ismael1361 <ismael1361@gmail.com>
2021-10-08 18:12:40 -0300

    202110081812

--- controllers/YoutubeMp3/index.js
+++ controllers/YoutubeMp3/index.js
@@ -4,18 +4,23 @@ const ytdl = require('ytdl-core');
 
 module.exports = async (req, res)=>{
     try{
-        const { media_id } = req.query;
+        const { media_id, download } = req.query;
 
         let url = 'https://music.youtube.com/watch?v='+media_id;
 
         if(ytdl.validateURL(url)){
             const info = await ytdl.getBasicInfo(url);
 
-            let videoReadableStream = ytdl(url, { filter: 'audioonly', quality: [18, 134, 140] });
+            let videoReadableStream = ytdl(url, { filter: 'audioonly', quality: [18, 140] });
 
             videoReadableStream.on("response", response => {
                 for(let k in response.headers){
                     res.setHeader(k, response.headers[k]);
+                }
+
+                if(Number(download === "" ? 1 : download)){
+                    let f = (/webm$/gi).test(response.headers["content-type"]) ? ".webm" : ".mp3";
+                    res.setHeader('Content-disposition', 'attachment; filename=music_' + media_id + f);
                 }
             });
 
